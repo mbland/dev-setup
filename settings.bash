53833feba3d99db4cef0abdc51b79252d1b0ed63
Mike Bland <mbland@acm.org>
2016-12-12 14:22:16 -0500

    Change 'install tools' to 'install packages'
    `lib/platform` sets `PLATFORM_*` variables based on `/etc/os-release` if
    present; otherwise relies on `OSTYPE`. See:
    https://www.freedesktop.org/software/systemd/man/os-release.html
    
    `lib/packages` keeps the `install packages` command script lean and
    well-organized, as well as makes it clear what platforms are supported.
    The hook functions distinguish which os-specific setup steps are
    happening, and which steps are specific to virtual machine
    installations.

--- settings.bash
+++ settings.bash
@@ -1,4 +1,37 @@
-declare -r APP_SYS_ROOT='/usr/local/mbland'
+# Root directory for language manager installs.
+declare -r APPS_ROOT='/usr/local/mbland'
+
+# List of language for which to install language managers.
+declare -r INSTALL_LANGUAGES=('go')
+
+# These are the packages that should get installed regardless of whether they're
+# already included in the OS.
+declare -r PACKAGES=(
+  'git'
+  'jq'
+  'mercurial'
+  'tmux'
+  'vim'
+)
+
+# These are the packages that should get installed only if not included in the
+# OS already.
+declare -r PACKAGES_ADD_IF_MISSING=(
+  'bison'
+  'bzip2'
+  'clang'
+  'curl'
+  'file'
+  'gcc'
+  'less'
+  'make'
+  'man'
+  'openssl'
+  'perl'
+  'tput'     # Test for for ncurses tools
+  'sqlite3'
+  'xz'
+)
 
 declare -r VIM_SCRIPTS=(
   'scripts/closetag.vim|http://www.vim.org/scripts/download_script.php?src_id=4318'
